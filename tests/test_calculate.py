2afbd7fe695acad944142ef2157d0f13c02dc718
HuShuhan <873933169@qq.com>
2024-06-24 16:52:08 +0800

    main: Added multiply and divide method

--- tests/test_calculate.py
+++ tests/test_calculate.py
@@ -1,6 +1,6 @@
 # tests/test_calculate.py
 import unittest
-from yukiiiii_tools.calculate import add, subtract
+from yukiiiii_tools.calculate import add, subtract, multiply, divide
 
 class TestCalculate(unittest.TestCase):
     def test_add(self):
@@ -8,3 +8,9 @@ class TestCalculate(unittest.TestCase):
 
     def test_subtract(self):
         self.assertEqual(subtract(3, 1), 2)
+
+    def test_multiply(self):
+        self.assertEqual(multiply(2, 3), 6)
+
+    def test_divide(self):
+        self.assertEqual(divide(3, 3), 1)

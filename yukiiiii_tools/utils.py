62f835fbe8a464199423b771e42f5c4340d327cf
HuShuhan <873933169@qq.com>
2024-07-17 15:04:08 +0800

    utils深度学习工具包更新

--- yukiiiii_tools/utils.py
+++ yukiiiii_tools/utils.py
@@ -1,7 +1,22 @@
-class AverageMeter(object):
-    """Computes and stores the average and current value"""
+import os
+import sys
+import random
+import errno
+import time
+import torch
+import numpy as np
+from datetime import timedelta
+
+class AverageMeter:
+    """
+    Computes and stores the average and current value
+    """
+
     def __init__(self):
-        self.reset()
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
 
     def reset(self):
         self.val = 0
@@ -9,8 +24,140 @@ class AverageMeter(object):
         self.sum = 0
         self.count = 0
 
-    def update(self, val, n=1):
+    def update(self, val):
         self.val = val
-        self.sum += val * n
-        self.count += n
+        self.sum += val
+        self.count += 1
         self.avg = self.sum / self.count
+
+def setup_system(seed, cudnn_benchmark=True, cudnn_deterministic=True) -> None:
+    '''
+    Set seeds for for reproducible training
+    '''
+    # python
+    random.seed(seed)
+    
+    # numpy
+    np.random.seed(seed)
+    
+    # pytorch
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    if torch.cuda.is_available():
+        torch.backends.cudnn_benchmark_enabled = cudnn_benchmark
+        torch.backends.cudnn.deterministic = cudnn_deterministic
+
+
+def mkdir_if_missing(dir_path):
+    try:
+        os.makedirs(dir_path)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
+class Logger(object):
+    def __init__(self, fpath=None):
+        self.console = sys.stdout
+        self.file = None
+        if fpath is not None:
+            mkdir_if_missing(os.path.dirname(fpath))
+            self.file = open(fpath, 'w')
+
+    def __del__(self):
+        self.close()
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, *args):
+        self.close()
+
+    def write(self, msg):
+        self.console.write(msg)
+        if self.file is not None:
+            self.file.write(msg)
+
+    def flush(self):
+        self.console.flush()
+        if self.file is not None:
+            self.file.flush()
+            os.fsync(self.file.fileno())
+
+    def close(self):
+        self.console.close()
+        if self.file is not None:
+            self.file.close()
+
+
+def sec_to_min(seconds):
+    
+    seconds = int(seconds)
+    minutes = seconds // 60
+    seconds_remaining = seconds % 60
+    
+    if seconds_remaining < 10:
+        seconds_remaining = '0{}'.format(seconds_remaining)
+    
+    return '{}:{}'.format(minutes, seconds_remaining)
+
+def sec_to_time(seconds):
+    return "{:0>8}".format(str(timedelta(seconds=int(seconds))))
+
+def print_time_stats(t_train_start, t_epoch_start, epochs_remaining, steps_per_epoch):
+    
+    elapsed_time = time.time() - t_train_start
+    speed_epoch = time.time() - t_epoch_start 
+    speed_batch = speed_epoch / steps_per_epoch
+    eta = speed_epoch * epochs_remaining
+        
+    print("Elapsed {}, {} time/epoch, {:.2f} s/batch, remaining {}".format(
+                sec_to_time(elapsed_time), sec_to_time(speed_epoch), speed_batch, sec_to_time(eta)))
+    
+
+def xyxy2xywh(x):  # Convert bounding box format from [x1, y1, x2, y2] to [x, y, w, h]
+    #y = torch.zeros(x.shape) if x.dtype is torch.float32 else np.zeros(x.shape)
+    y = torch.zeros_like(x)
+    y[:, 0] = (x[:, 0] + x[:, 2]) / 2
+    y[:, 1] = (x[:, 1] + x[:, 3]) / 2
+    y[:, 2] = x[:, 2] - x[:, 0]
+    y[:, 3] = x[:, 3] - x[:, 1]
+    return y
+
+def xywh2xyxy(x):  # Convert bounding box format from [x, y, w, h] to [x1, y1, x2, y2]
+    #y = torch.zeros(x.shape) if x.dtype is torch.float32 else np.zeros(x.shape)
+    y = torch.zeros_like(x)
+    y[:, 0] = (x[:, 0] - x[:, 2] / 2)
+    y[:, 1] = (x[:, 1] - x[:, 3] / 2)
+    y[:, 2] = (x[:, 0] + x[:, 2] / 2)
+    y[:, 3] = (x[:, 1] + x[:, 3] / 2)
+    return y
+
+def bbox_iou(box1, box2, x1y1x2y2=True):
+    """
+    Returns the IoU of two bounding boxes
+    """
+    if x1y1x2y2:
+        # Get the coordinates of bounding boxes
+        b1_x1, b1_y1, b1_x2, b1_y2 = box1[:, 0], box1[:, 1], box1[:, 2], box1[:, 3]
+        b2_x1, b2_y1, b2_x2, b2_y2 = box2[:, 0], box2[:, 1], box2[:, 2], box2[:, 3]
+    else:
+        # Transform from center and width to exact coordinates
+        b1_x1, b1_x2 = box1[:, 0] - box1[:, 2] / 2, box1[:, 0] + box1[:, 2] / 2
+        b1_y1, b1_y2 = box1[:, 1] - box1[:, 3] / 2, box1[:, 1] + box1[:, 3] / 2
+        b2_x1, b2_x2 = box2[:, 0] - box2[:, 2] / 2, box2[:, 0] + box2[:, 2] / 2
+        b2_y1, b2_y2 = box2[:, 1] - box2[:, 3] / 2, box2[:, 1] + box2[:, 3] / 2
+
+    # get the coordinates of the intersection rectangle
+    inter_rect_x1 = torch.max(b1_x1, b2_x1)
+    inter_rect_y1 = torch.max(b1_y1, b2_y1)
+    inter_rect_x2 = torch.min(b1_x2, b2_x2)
+    inter_rect_y2 = torch.min(b1_y2, b2_y2)
+    # Intersection area
+    inter_area = torch.clamp(inter_rect_x2 - inter_rect_x1, 0) * torch.clamp(inter_rect_y2 - inter_rect_y1, 0)
+    # Union Area
+    b1_area = (b1_x2 - b1_x1) * (b1_y2 - b1_y1)
+    b2_area = (b2_x2 - b2_x1) * (b2_y2 - b2_y1)
+
+    # print(box1, box1.shape)
+    # print(box2, box2.shape)
+    return inter_area / (b1_area + b2_area - inter_area + 1e-16)
